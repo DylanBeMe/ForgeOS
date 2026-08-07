5c2e679adfd47f92508b28992e6f140ff8527106
RandomCatboy <dylan@youhavethe.gay>
2026-08-07 17:45:02 +0200

    Patch install-deps.sh to install font "fonts-dejavu-core" to fix build issue.

--- tools/ci/install-deps.sh
+++ tools/ci/install-deps.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
   bc bison build-essential busybox btrfs-progs clang cmake cpio file flex git \
-  imagemagick libncurses-dev libpython3-dev libsdl-ttf2.0-dev libsdl1.2-dev \
+  fonts-dejavu-core imagemagick libncurses-dev libpython3-dev libsdl-ttf2.0-dev \
+  libsdl1.2-dev \
   libssl-dev mercurial patch pkg-config python3 rsync shellcheck subversion \
   swig unzip wget xz-utils zip
