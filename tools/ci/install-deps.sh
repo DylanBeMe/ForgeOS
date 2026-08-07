6aed0390a5fde3c14e0dc0ad4ec014ae93b3019e
RandomCatboy <dylan@youhavethe.gay>
2026-08-07 20:48:20 +0200

    Retry CI CD font fix

--- tools/ci/install-deps.sh
+++ tools/ci/install-deps.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
   bc bison build-essential busybox btrfs-progs clang cmake cpio file flex git \
-  fonts-dejavu-core imagemagick libncurses-dev libpython3-dev libsdl-ttf2.0-dev \
+  fonts-urw-base35 imagemagick libncurses-dev libpython3-dev libsdl-ttf2.0-dev \
   libsdl1.2-dev \
   libssl-dev mercurial patch pkg-config python3 rsync shellcheck subversion \
   swig unzip wget xz-utils zip
